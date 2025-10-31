aeb510cdc1c905ab127baeea254ef3fb14a55802
rezucos <tsyganok.vasya@proton.me>
2025-10-31 11:05:43 +0100

    APP_URL_ID update

--- src/shared/constants/client.ts
+++ src/shared/constants/client.ts
@@ -1,6 +1,6 @@
 "use client";
 
 export const API_URL = "https://dev.tele.store:8081";
-export const APP_URL_ID = 1740742094035316;
+export const APP_URL_ID = 1758874298367440011;
 export const APP_ID = 1739197350166203;
 export const PUBLIC_USER_KEY = "d6nVr9t4VklHNF9kGk0e2CwuyncgzAkwfE9gAtG6Efk";
