97a5e6f6520aa3f331ae9b21a335538d83e569c4
rezucos <tsyganok.vasya@proton.me>
2025-12-09 10:15:23 +0100

    public key update

--- src/shared/constants/client.ts
+++ src/shared/constants/client.ts
@@ -3,7 +3,7 @@
 export const API_URL: string = "https://dev.tele.store:8081";
 export const APP_URL_ID: string = "1758874298367440011";
 export const APP_ID: string = "1739197350166203";
-export const PUBLIC_USER_KEY: string = "d6nVr9t4VklHNF9kGk0e2CwuyncgzAkwfE9gAtG6Efk";
+export const PUBLIC_USER_KEY: string = "x_FtUrhBftWeZmAKhj4RFssYm2qL2fGySbD_JpgF-_c";
 
 export function getBaseUrl(): string {
     const proxyId = localStorage.getItem("proxy_id");
